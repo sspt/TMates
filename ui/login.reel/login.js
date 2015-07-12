c16690f8a980a23ea5e6b3dee8851e9582dbb97e
sspt <noreply>
2015-07-12 16:24:38 +0000

    Add component login

---
+++ ui/login.reel/login.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/login.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Login
+ * @extends Component
+ */
+exports.Login = Component.specialize(/** @lends Login# */ {
+    constructor: {
+        value: function Login() {
+            this.super();
+        }
+    }
+});
